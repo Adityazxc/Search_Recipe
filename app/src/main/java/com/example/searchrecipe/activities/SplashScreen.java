8ef6cad6643c130c73c55b6046937ffcfe56d6eb
Adityazxc <adityaads623@gmail.com>
2022-08-11 08:05:11 +0700

    Update 3.1

---
+++ app/src/main/java/com/example/searchrecipe/activities/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.searchrecipe.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.searchrecipe.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            finish();
+        },1000);
+
+    }
+}
